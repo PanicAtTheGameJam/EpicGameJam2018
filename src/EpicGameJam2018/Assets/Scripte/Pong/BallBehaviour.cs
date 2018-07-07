5b74166863b1a4493e14445c28fd1c84143543ff
Unknown <tavernoshca@gmail.com>
2018-07-08 01:18:11 +0200

    Add speed control

---
+++ src/EpicGameJam2018/Assets/Scripte/Pong/BallBehaviour.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallBehaviour : MonoBehaviour {
+    public float speedLeft { private set; get; }
+    public float speedRight { private set; get; }
+
+    public float Speed
+    {
+        get
+        {
+            return (speedLeft + speedRight) / 2;
+        }
+    }
+
+    void Start()
+    {
+        speedLeft = 15;
+        speedRight = 15;
+
+        System.Random random = new System.Random();
+        int x = random.Next(100);
+
+        Debug.Log(x);
+        GetComponent<Rigidbody2D>().velocity = (x > 50 ? Vector2.left : Vector2.right) * Speed;
+    }
+
+    void Update()
+    {
+        Debug.Log(Speed);
+        GetComponent<Rigidbody2D>().velocity = GetComponent<Rigidbody2D>().velocity.normalized * Speed;
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        // Note: 'col' holds the collision information. If the
+        // Ball collided with a racket, then:
+        //   col.gameObject is the racket
+        //   col.transform.position is the racket's position
+        //   col.collider is the racket's collider
+
+        // Hit the left Racket?
+        if (col.gameObject.name == "RacketLeft")
+        {
+            // Calculate hit Factor
+            float y = HitFactor(transform.position,
+                                col.transform.position,
+                                col.collider.bounds.size.y);
+
+            // Calculate direction, make length=1 via .normalized
+            Vector2 dir = new Vector2(1, y).normalized;
+
+            // Set Velocity with dir * speed
+            GetComponent<Rigidbody2D>().velocity = dir * Speed;
+        }
+
+        // Hit the right Racket?
+        if (col.gameObject.name == "RacketRight")
+        {
+            // Calculate hit Factor
+            float y = HitFactor(transform.position,
+                                col.transform.position,
+                                col.collider.bounds.size.y);
+
+            // Calculate direction, make length=1 via .normalized
+            Vector2 dir = new Vector2(-1, y).normalized;
+
+            // Set Velocity with dir * speed
+            GetComponent<Rigidbody2D>().velocity = dir * Speed;
+        }
+    }
+
+    private float HitFactor(Vector2 ballPos, Vector2 racketPos,
+                float racketHeight)
+    {
+        // ascii art:
+        // ||  1 <- at the top of the racket
+        // ||
+        // ||  0 <- at the middle of the racket
+        // ||
+        // || -1 <- at the bottom of the racket
+        return (ballPos.y - racketPos.y) / racketHeight;
+    }
+
+    public void SetSpeedLeft(float speed)
+    {
+        speedLeft = speed;
+    }
+
+    public void SetSpeedRight(float speed)
+    {
+        speedRight = speed;
+    }
+}
---
+++ src/EpicGameJam2018/Assets/Scripte/Pong/SpeedControls.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Side { left, right }
+
+public class SpeedControls : MonoBehaviour {
+    private readonly float MAX = 50;
+    private readonly float MIN = 5;
+    private readonly float STEPS = 0.5f;
+
+    public GameObject gameManagerObject;
+    public GameObject ballObject;
+    public PlayerId playerId;
+    public Side side;
+
+    private GameManager gameManager;
+    private BallBehaviour ballBehaviour;
+
+	void Start () {
+        gameManager = gameManagerObject.GetComponent<GameManager>();
+        ballBehaviour = ballObject.GetComponent<BallBehaviour>();
+
+        Dictionary<KeyCallback, Action> callbacks = new Dictionary<KeyCallback, Action>();
+        callbacks.Add(KeyCallback.KeyOnePressed, OnKeyOnePressed);
+        callbacks.Add(KeyCallback.KeyTwoPressed, OnKeyTwoPressed);
+
+        gameManager.SetActionsForPlayer(playerId, callbacks);
+	}
+
+    private void OnKeyOnePressed()
+    {
+        if (side == Side.left)
+        {
+            if (ballBehaviour.speedLeft > MIN)
+            {
+                ballBehaviour.SetSpeedLeft(ballBehaviour.speedLeft - STEPS);
+            }
+        }
+        else
+        {
+            if (ballBehaviour.speedRight > MIN)
+            {
+                ballBehaviour.SetSpeedRight(ballBehaviour.speedRight - STEPS);
+            }
+        }
+    }
+	
+    private void OnKeyTwoPressed()
+    {
+        if(side == Side.left)
+        {
+            if(ballBehaviour.speedLeft < MAX)
+            {
+                ballBehaviour.SetSpeedLeft(ballBehaviour.speedLeft + STEPS);
+            }
+        }
+        else
+        {
+            if (ballBehaviour.speedRight < MAX)
+            {
+                ballBehaviour.SetSpeedRight(ballBehaviour.speedRight + STEPS);
+            }
+        }
+    }
+}
